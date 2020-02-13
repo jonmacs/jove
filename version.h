cbcb072c900e7efd22e52fd2ed88130ac75ec679
Mark Moraes <moraes@computer.org>
2020-02-12 23:25:11 -0500

    4.17.1.11: to catch .gitattributes (deleted pre-release 4.17.1.10)

--- version.h
+++ version.h
@@ -5,5 +5,5 @@
  * this notice is included in all the source files and documentation.     *
  **************************************************************************/
 
-#define jversion	"4.17.1.10"
-#define jversion_lnum	4,17,1,10	/* for windows jjove.rc file */
+#define jversion	"4.17.1.11"
+#define jversion_lnum	4,17,1,11	/* for windows jjove.rc file */
